535b6fbe2de8ca4d3f738d821f1df0da4bebaa6c
Marcio Reis <dev.marcioreis@gmail.com>
2023-01-16 16:53:42 -0300

    navbar click's js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,14 @@
+const navbar = document.querySelector('.navbar')
+const navbarLinks = document.querySelector('.navbar-link')
+const menuToggleBtn = document.querySelector('.nav-toggle-btn')
 
+menuToggleBtn.addEventListener('click', function() {
+    navbar.classList.toggle('active')
+    this.classList.toggle('active')
+});
+
+navbar.addEventListener('click', () => {
+    navbar.classList.remove('active')
+    menuToggleBtn.classList.toggle('active')
+})
 
