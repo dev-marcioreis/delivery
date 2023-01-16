687674f02f7667f68e3bb10dc46a18c3d5d96740
Marcio Reis <dev.marcioreis@gmail.com>
2023-01-16 17:13:13 -0300

    header color js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,12 @@ navbar.addEventListener('click', () => {
     menuToggleBtn.classList.toggle('active')
 })
 
+const header = document.querySelector('.header')
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY >= 100) {
+        header.classList.add('active')
+    } else {
+        header.classList.remove('active')
+    }
+})
